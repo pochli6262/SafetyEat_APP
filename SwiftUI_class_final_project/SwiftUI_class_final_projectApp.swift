768fc8dab2ebed6a7bed27811149ef5c4fbc8000
jason <pochli6262@gmail.com>
2025-06-01 21:07:24 +0800

    Initial Commit

---
+++ SwiftUI_class_final_project/SwiftUI_class_final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_class_final_projectApp.swift
+//  SwiftUI_class_final_project
+//
+//  Created by 廖柏鈞 on 2025/6/1.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_class_final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
